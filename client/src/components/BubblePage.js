6916bbae5e8c50cb24aaf838d334245fbcd1ffde
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-06-19 10:45:27 -0500

    BubblePage getting list from api on init render

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,13 +1,28 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWIthAuth';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then((res) => {
+        // console.log('BubblePage -> res.data', res.data);
+        setColorList(res.data)
+      })
+      .catch((err) => {
+        console.log(
+          'BubblePage -> err',
+          err.response.status,
+          err.response.statusText
+        );
+      });
+  }, []);
 
   return (
     <>
